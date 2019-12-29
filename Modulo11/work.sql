d07f4866c6f4c0e63766ba5bff58e5cafb939c4d
Gustavo Coronel <gcoronelc@gmail.com>
2019-12-29 10:35:27 -0500

    Modulo 11

--- Modulo11/work.sql
+++ Modulo11/work.sql
@@ -43,6 +43,41 @@ where empno = 67890;
 update scott.emp
 set sal = sal + 100;
 
-drop trigger scott.tr_demo_01;
+drop trigger scott.tr_demo_02;
 
+
+
+
+-- FUNCIONES LOGICAS
+
+CREATE OR REPLACE TRIGGER SCOTT.tr_DEMO_03
+AFTER INSERT OR DELETE OR UPDATE ON SCOTT.emp
+BEGIN
+  if inserting then
+    dbms_output.put_line( 'nuevo empleado se ha insertado' );
+  Elsif updating then
+    dbms_output.put_line( 'un empleado se ha modificado' );
+  Elsif deleting then
+    dbms_output.put_line( 'un empleado se ha eliminado' );
+  end if;
+  IF UPDATING('SAL') THEN
+    dbms_output.put_line( 'HA CAMBIADO EL SALARIO' );
+  END IF;
+END tr_DEMO_03;
+/
+
+
+INSERT INTO SCOTT.EMP(EMPNO,ENAME,SAL)
+VALUES(7878,'GUSTAVO',6767);
+
+UPDATE SCOTT.EMP
+SET SAL = 7676
+WHERE EMPNO = 7878;
+
+UPDATE SCOTT.EMP
+SET ENAME = 'ANGEL'
+WHERE EMPNO = 7878;
+
+DELETE SCOTT.EMP 
+WHERE EMPNO = 7878;
 
